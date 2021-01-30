923a246ebadbdda17e262bcf76d649856f12ac47
alanmark13579 <korosu1128@gmail.com>
2021-01-30 22:39:49 +0800

    add view binding

--- AndroidInterview/app/src/main/java/trans/example/androidinterview/FirstFragment.kt
+++ AndroidInterview/app/src/main/java/trans/example/androidinterview/FirstFragment.kt
@@ -1,33 +1,39 @@
 package trans.example.androidinterview
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_first.*
 import trans.example.androidinterview.R.layout.fragment_first
+import trans.example.androidinterview.databinding.FragmentFirstBinding
 
 class FragmentFirst : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
+    private var Firstbinding: FragmentFirstBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
     ): View? {
         return inflater.inflate(fragment_first, container, false)
     }
-
     override fun onViewCreated(
         view: View,
-        savedInstanceState: Bundle?, ) {
-        super.onViewCreated(view, savedInstanceState)
-
+        savedInstanceState: Bundle?,
+    ) {
+        val binding = FragmentFirstBinding.bind(view)
+        Firstbinding = binding
         val manager = activity!!.supportFragmentManager
         val transaction = manager.beginTransaction()
-        super.onViewCreated(view, savedInstanceState)
-        val switchButton = SwitchButton
-        switchButton.setOnClickListener {
+        val switchButton = binding.SwitchButton
+        switchButton.setOnClickListener() {
             val fragment2 = FragmentSecond()
             transaction.replace(R.id.center, fragment2)
             transaction.commit()
         }
     }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Firstbinding = null
+    }
+
 }
--- AndroidInterview/app/src/main/java/trans/example/androidinterview/FragmentSecond.kt
+++ AndroidInterview/app/src/main/java/trans/example/androidinterview/FragmentSecond.kt
@@ -2,31 +2,98 @@ package trans.example.androidinterview
 
 import android.R
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.bumptech.glide.load.model.GlideUrl
-import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_second.*
 import kotlinx.android.synthetic.main.recycle_item.*
-import trans.example.androidinterview.R.layout.fragment_second
+import trans.example.androidinterview.databinding.FragmentFirstBinding
+import trans.example.androidinterview.databinding.FragmentSecondBinding
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
+var arrayList: ArrayList<HashMap<String, String>> = ArrayList()
 
+// 測試RecyclerView的code
 class FragmentSecond : Fragment()
 {
+    var TAG = "mExample"
     var mRecyclerView: RecyclerView? = null
-//    private var myListAdapter: MyListAdapter? = null
-    public var arrayList: ArrayList<HashMap<String, String>> = ArrayList()
+    var myListAdapter: MyListAdapter? = null
+    private var Secondbinding: FragmentSecondBinding? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(trans.example.androidinterview.R.layout.fragment_second, container, false)
+    }
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?, ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //製造資料
+        for (i in 0..29) {
+            val hashMap: HashMap<String, String> = HashMap()
+            hashMap["Id"] = "座號：" + String.format("%02d", i + 1)
+            hashMap["Sub1"] = java.lang.String.valueOf(Random().nextInt(80) + 20)
+            hashMap["Sub2"] = java.lang.String.valueOf(Random().nextInt(80) + 20)
+            hashMap["Avg"] = (
+                    (hashMap["Sub1"]!!.toInt() + hashMap["Sub2"]!!.toInt()) / 2).toString()
+            arrayList.add(hashMap)
+        }
+        //設置RecycleView
+        mRecyclerView = recyclerview
+        mRecyclerView!!.layoutManager = LinearLayoutManager(this.context)
+        mRecyclerView!!.addItemDecoration(DividerItemDecoration(this.context,
+            DividerItemDecoration.VERTICAL))
+        myListAdapter = MyListAdapter()
+        mRecyclerView!!.adapter = myListAdapter*/
+
+
+    } //onCreate
+   class MyListAdapter : RecyclerView.Adapter<MyListAdapter.ViewHolder>() {
+       val binding = FragmentFirstBinding.bind(view)
+       Secondbinding = binding
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val tvId: TextView = itemView.findViewById<View>(R.id.textView_Id) as TextView
+            val tvSub1: TextView = itemView.findViewById(R.id.textView_sub1)
+            val tvSub2: TextView = itemView.findViewById(R.id.textView_sub2)
+            val tvAvg: TextView = itemView.findViewById(R.id.textView_avg)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view: View = LayoutInflater.from(parent.context)
+                .inflate(R.layout.recycle_item, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+           // val avgS: Int = arrayList.get(position).get("Avg").toInt()
+
+            holder.tvId.setText(arrayList.get(position).get("Id"))
+            holder.tvSub1.setText(arrayList.get(position).get("Sub1"))
+            holder.tvSub2.setText(arrayList.get(position).get("Sub2"))
+            holder.tvAvg.setText(arrayList.get(position).get("Avg"))
+        }
+
+        override fun getItemCount(): Int {
+            return arrayList.size
+        }
+    }
+
+}
+/*  從url抓圖片下來的code
+{
+    var mRecyclerView: RecyclerView? = null
+    private var myListAdapter: MyListAdapter? = null
+var arrayList: ArrayList<HashMap<String, String>> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,65 +115,23 @@ class FragmentSecond : Fragment()
                 )
                 for (item in posts) {
                     val hashMap: HashMap<String, String> = HashMap()
-
-                    hashMap["Id"] = String.format("%02d", item.id)
-                    hashMap["Title"] = item.title.toString()
-                    hashMap["thumbnailUrl"] = item.thumbnailUrl.toString()
-                    //Log.d("HKT",  hashMap["thumbnailUrl"].toString() )
+                    hashMap["Id"] = String.format("%02d", posts[0].id)
+                    hashMap["Title"] = String.format("%02d", posts[0].title)
+                    hashMap["thumbnailUrl"] = String.format("%02d", posts[0].thumbnailUrl)
                     arrayList.add(hashMap);
-                }
 
+                }
             }) { error -> // 連線失敗
             Log.d("HKT", error.toString())
         }
         queue.add(stringRequest)
 
         val view = inflater.inflate(fragment_second, container, false)
-       /* val mRecyclerView = recyclerview
+        val mRecyclerView = recyclerview
         mRecyclerView.layoutManager = LinearLayoutManager(this.context)
-        mRecyclerView.addItemDecoration(
-            DividerItemDecoration(
-                this.context,
-                DividerItemDecoration.VERTICAL
-            )
-        )
+        mRecyclerView.addItemDecoration(DividerItemDecoration(this.context, DividerItemDecoration.VERTICAL))
         myListAdapter = MyListAdapter()
-        mRecyclerView.adapter = myListAdapter*/
+        mRecyclerView.adapter = myListAdapter
         return view
     }
-
-
-}
-/*
-private class MyListAdapter : RecyclerView.Adapter<MyListAdapter.ViewHolder>() {
-
-    internal class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val thumbnailUrl: ImageView = show_photo_1
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view: View = LayoutInflater.from(parent.context)
-            .inflate(
-                R.layout.recycle_item,
-                parent,
-                false
-            )
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val avgS: Int = arrayList.get(position).get("thumbnailUrl").toInt()
-
-        holder.thumbnailUrl.
-
-        val url = GlideUrl(
-            arrayList.get(position).get("thumbnailUrl")[0].thumbnailUrl, LazyHeaders.Builder()
-                .addHeader("User-Agent", "your-user-agent")
-                .build()
-        )
-        Glide.with(this).load(url).into(imageView);
-    }
-    override fun getItemCount(): Int {
-        return arrayList.size()
-    }
-}*/
+    */
